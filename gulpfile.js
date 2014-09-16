9214daa0b2c648396fc0c58defdbb81b1120201e
Tom Genoni <tom@atomeye.com>
2014-09-15 22:32:05 -0700

    Updating script.

--- gulpfile.js
+++ gulpfile.js
@@ -17,15 +17,16 @@ var paths = {
   sass: './scss/'
 };
 
-//  Copy Static Assets
+//  copy_assets: copy static assets
 //===========================================
 gulp.task('copy_assets', function() {
   gulp.src('assets/**/*')
     .pipe(gulp.dest('_site/assets/'))
     .pipe(livereload(server))
+    //.pipe(notify({ message: 'Assets Copied' }));
 });
 
-// fileinclude: grab partials from templates and render out html files
+// fileinclude: grab partials from templates and render html files
 // ==========================================
 gulp.task('fileinclude', function() {
   return gulp.src(path.join(paths.templates, '*.tpl.html'))
@@ -38,10 +39,10 @@ gulp.task('fileinclude', function() {
   }))
   .pipe(gulp.dest('./_site/'))
   .pipe(livereload(server))
-  .pipe(notify({ message: 'Includes: included' }));
+  //.pipe(notify({ message: 'Includes: included' }));
 });
 
-//  Sass: compile sass to css task - uses Libsass
+//  compass: compile sass to css
 //===========================================
 gulp.task('compass', function() {
   return gulp.src(path.join(paths.sass, '*.scss'))
@@ -51,7 +52,7 @@ gulp.task('compass', function() {
       sass: 'scss'
     }))
     .pipe(livereload(server))
-    .pipe(notify({ message: 'CSS Compiled' }));
+    //.pipe(notify({ message: 'CSS Compiled' }));
 });
 
 //  Connect: sever task
@@ -64,35 +65,20 @@ gulp.task('connect', function() {
   });
 });
 
-function watchStuff() {
-  // Listen on port other than 35729
-  server.listen(35728, function (err) {
-    if (err) {
-      return console.error(err)
-      //TODO use notify to log a message on Sass compile fail and Beep
-    };
-
-    //Watch task for sass
-    gulp.watch(path.join(paths.sass, '**/*.scss'), ['compass']);
-
-    // watch task for templates, partials, static files
-    gulp.watch(path.join(paths.templates, '**/*.html'), ['fileinclude']);
-    gulp.watch(path.join(paths.partials, '**/*.html'), ['fileinclude']);
-    gulp.watch(path.join(paths.assets, '**/*.{png,jpg,jpeg,css,js}'), ['copy_assets']);
-
-  });
-}
-
 //  Watch and Livereload using Compass
 //===========================================
 gulp.task('watch', function() {
 
-  watchStuff();
+  //Watch task for sass
+  gulp.watch(path.join(paths.sass, '**/*.scss'), ['compass']);
+
+  // watch task for templates, partials, static files
+  gulp.watch(path.join(paths.templates, '**/*.html'), ['fileinclude']);
+  gulp.watch(path.join(paths.partials, '**/*.html'), ['fileinclude']);
+  gulp.watch(path.join(paths.assets, '**/*.{png,jpg,jpeg,css,js}'), ['copy_assets']);
 
 });
 
 //  Default Gulp Task
 //===========================================
-gulp.task('default', ['fileinclude', 'compass', 'copy_assets', 'connect', 'watch'], function() {
-
-});
+gulp.task('default', ['fileinclude', 'compass', 'copy_assets', 'connect', 'watch']);
